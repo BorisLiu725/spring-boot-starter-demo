bfdafe4b1e77b524ce8c87da21605f128a6b4fb9
BorisLiu725 <2502644175@qq.com>
2020-03-21 13:09:45 +0800

    my starter

---
+++ ly-spring-boot-starter-autoconfigurer/src/main/java/com/ly/configuration/HelloServiceAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.ly.configuration;
+
+
+import com.ly.properties.HelloProperties;
+import com.ly.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication //web应用才生效
+@EnableConfigurationProperties(HelloProperties.class) //让对应的配置文件生效
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+
+    @Bean
+    public HelloService helloService(){
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+
+
+
+
+}
